95db8a0661c07093a7ba9d3545cb92e3c512bd09
yewonJin <jyw966@naver.com>
2022-04-26 01:16:43 +0900

    Todo_pacman-second commit

--- Todo_pacman/script.js
+++ Todo_pacman/script.js
@@ -1,9 +1,10 @@
-const $packman = document.querySelector('.pacman');
+const $pacman = document.querySelector('.pacman');
 
 var [pX, pY] = [0, 0];
 var deg = 0;
 var scale = 1;
 
+// Translate the pacman
 window.addEventListener('keydown', e => {
     if(e.key === 'ArrowUp') {
         if(pY === 0) return;
@@ -25,10 +26,13 @@ window.addEventListener('keydown', e => {
         console.log("다시 입력");
     }
 
-    $packman.style.transform = `translateY(${pY}px) rotate(${deg}deg) scaleX(${scale})`;
+    $pacman.style.transform = `translateY(${pY}px) rotate(${deg}deg) scaleX(${scale})`;
 });
 
-$packman.addEventListener('click', e => {
+// Remove the content
+
+
+$pacman.addEventListener('click', e => {
     var count = pY/60;
 
     if((count * 2) % 2 === 1) return; 
@@ -42,18 +46,49 @@ $packman.addEventListener('click', e => {
         count += 1;
     }
 
-    for(let i = 0; i < 5; i++) {
-        setTimeout(() => {
-            pX -= 20;
-            e.target.style.transform = `translateX(${pX}px) rotate(${deg}deg) scaleX(${scale})`;
-        }, 450)
-
-        setTimeout(() => {
-            $packman.style.setProperty('--after', 0);
-        }, 500)
-
-        setTimeout(() => {
-            $packman.style.setProperty('--after', 1);
-        }, 650)
+    // content
+    try {
+        document.querySelector('.content').children[0].children[count].classList.toggle('remove');
+    } catch (error) {
+        console.log("해당 컨텐트가 없습니다.");
     }
+
+    var timerCount = 0;
+
+    const promise = () => {
+        return new Promise((resolve) => {
+            const timer = setInterval(() => {
+                if(timerCount > 6) {
+                    clearTimeout(timer);
+                    resolve();
+                }
+        
+                setTimeout(() => {
+                    pX -= 40;
+                    e.target.style.transform = `translateX(${scale > 0 ? pX: -pX}px) translateY(${pY}px) rotate(${deg}deg) scaleX(${scale})`;
+                }, 0)
+            
+                setTimeout(() => {
+                    $pacman.style.setProperty('--after', 0);
+                }, 300)
+            
+                setTimeout(() => {
+                    $pacman.style.setProperty('--after', 1);
+                }, 500)
+        
+                timerCount++;
+                console.log(timerCount);
+
+
+            }, 800);
+        });
+    }
+
+    promise().then(() => {
+        document.querySelector('.content').children[0].removeChild(document.querySelector('.content').children[0].children[count]);
+        pX = 40; pY= 0;
+        document.querySelector('.pacman').style.transform = `translateX(${pX}) translateY(${pY}) rotate(${deg}deg) scaleX(${scale})`
+    });
+        
 });
+
