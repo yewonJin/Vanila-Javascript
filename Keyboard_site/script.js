0c7cd4dd379851e6371e40f31577511df295f906
yewonJin <jyw966@naver.com>
2022-05-02 23:05:18 +0900

    Keyboard_site : second commit

--- Keyboard_site/script.js
+++ Keyboard_site/script.js
@@ -1,22 +1,32 @@
 const $page =  document.querySelector('.page');
 
+var sliderCount = 0;
+
 window.addEventListener('load', () => {
     setInterval(() => {
-        document.querySelector('.image').style.transform = `translateX(${-905*index}px)`;
-    }, 2000)
+        if(sliderCount === 3) sliderCount -= 4;
+        [...$page.children].forEach(item => {
+            item.style.backgroundColor = "";
+        });
+        document.querySelector('.page').children[sliderCount+1].style.backgroundColor = "grey";
+        document.querySelector('.image').style.transform = `translateX(${-100*++sliderCount}%)`;
+    }, 5000) 
+   
 })
 
 $page.addEventListener('click', e => {
     if(e.target.tagName !== 'LI') return;
+    initialPageSetting(e);
+});
 
+
+function initialPageSetting(e) {
     [...$page.children].forEach((item, index) => {
-        item.style.backgroundColor = "#cccccc";
-
+        item.style.backgroundColor = "";
         if(item === e.target) {
-            document.querySelector('.image').style.transform = `translateX(${-905*index}px)`;
+            sliderCount = index;
+            document.querySelector('.image').style.transform = `translateX(${-100*sliderCount}%)`;
         }
     })
-
-    e.target.style.backgroundColor = "white";
-
-});
+    document.querySelector('.page').children[sliderCount].style.backgroundColor = "grey";
+}
