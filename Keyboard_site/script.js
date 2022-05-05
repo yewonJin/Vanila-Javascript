c1263bb6a295e613dbe58f837c0691e44cd3f692
yewonJin <jyw966@naver.com>
2022-05-05 23:14:39 +0900

    Keyboard_site : third commit

--- Keyboard_site/script.js
+++ Keyboard_site/script.js
@@ -2,23 +2,24 @@ const $page =  document.querySelector('.page');
 
 var sliderCount = 0;
 
+// Slider Animation Timer
 window.addEventListener('load', () => {
     setInterval(() => {
         if(sliderCount === 3) sliderCount -= 4;
         [...$page.children].forEach(item => {
             item.style.backgroundColor = "";
         });
-        document.querySelector('.page').children[sliderCount+1].style.backgroundColor = "grey";
+        document.querySelector('.page').children[sliderCount+1].style.backgroundColor = "white";
         document.querySelector('.image').style.transform = `translateX(${-100*++sliderCount}%)`;
     }, 5000) 
    
 })
 
+// Slider Animation Click
 $page.addEventListener('click', e => {
     if(e.target.tagName !== 'LI') return;
     initialPageSetting(e);
 });
-
 
 function initialPageSetting(e) {
     [...$page.children].forEach((item, index) => {
@@ -28,5 +29,27 @@ function initialPageSetting(e) {
             document.querySelector('.image').style.transform = `translateX(${-100*sliderCount}%)`;
         }
     })
-    document.querySelector('.page').children[sliderCount].style.backgroundColor = "grey";
+    document.querySelector('.page').children[sliderCount].style.backgroundColor = "white";
 }
+
+// Nav-Search Animation
+const $search = document.getElementById('search_icon');
+
+$search.addEventListener('click', e => {
+    if(e.target.parentNode.className === "search_li open") {
+        document.querySelector('.search').classList.toggle('open');
+        e.target.parentNode.classList.toggle('open');
+    } else{
+        setTimeout(() => {
+            document.querySelector('.search').classList.toggle('open');
+        }, 800);
+        e.target.parentNode.classList.toggle('open');
+    }
+})
+
+// Global_narrow_nav
+const $menu = document.getElementById('menu');
+
+$menu.addEventListener('click', e => {
+    document.querySelector('.global_nav_menu').classList.toggle('open');
+});
